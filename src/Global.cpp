52b2cb6eac6f742382c69dbe6886e25c0de64792
DisesFgewu <martin0968872985@gmail.com>
2025-04-25 16:22:22 +0800

    Deal With RainBow Problem

--- src/Global.cpp
+++ src/Global.cpp
@@ -8,7 +8,7 @@ std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now
 int is_click = 0;
 
 int item_num[3] = {
-    5, 3, 2
+    5, 3, 20
 };
 
 glm::vec2 item_position[3] {
