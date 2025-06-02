f7bb0b21f9822b38d0da32b5cc9a6b46bffd9a09
Laiyuhung <laiyuhung921118@gmail.com>
2025-06-02 11:24:49 +0800

    0602

--- src/Global.cpp
+++ src/Global.cpp
@@ -8,7 +8,7 @@ std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now
 int is_click = 0;
 
 int item_num[3] = {
-    10, 12, 10
+    100, 100, 100
 };
 
 glm::vec2 item_position[3] {
